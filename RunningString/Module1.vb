a9df761c859d489702851dabcb3e84228e700e02
Alexander <pchp250g8@tutamail.com>
2025-12-25 12:47:02 +0300

    Добавьте файлы проекта.

---
+++ RunningString/Module1.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Threading
+
+Module Module1
+
+	Sub Main()
+		Console.Clear()
+		Console.CursorVisible = False
+		Dim strSpaces = New String(" "c, 80)
+		Dim strLine = "Hello World!!! Running String!!! Press Any Key To Exit!!!"
+		strLine = strSpaces & strLine & strSpaces
+		Dim nCounter = 0
+		Dim nStrLen = strLine.Length - 80
+		Do
+			strSpaces = strLine.Substring(nCounter, 80)
+			Console.SetCursorPosition(10, 10)
+			Console.Write(strSpaces)
+			Thread.Sleep(140)
+			nCounter += 1
+			If nCounter >= nStrLen Then
+				nCounter = 0
+			End If
+		Loop While Not Console.KeyAvailable
+	End Sub
+
+End Module
---
+++ RunningString/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("RunningString")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("RunningString")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("992d66f2-293a-4ad0-b0e6-c29f83a4461a")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
